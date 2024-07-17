1746f763bcec6c316e69f843c5ace390280994c4
Lisa Chukwu <chukwu.liza@gmail.com>
2024-07-17 20:11:01 +0100

    Updates

--- pex.js
+++ pex.js
@@ -1,3 +1,5 @@
+//this is an assignment 
+
 function tod(time) {
     console.log('This function is to note the time of day')
     if (time === 'morning') {
@@ -11,6 +13,10 @@ function tod(time) {
     }
 }
 
+//calling the function
+tod('morning');
+tod('Afternoon')
+
 function login(password) {
     
     let new_password = 2345
@@ -23,3 +29,30 @@ function login(password) {
         console.log("You are a thief")
     }
 }
+
+//using the return
+function login(password) {
+    
+    let new_password = 2345
+    var old_password = 1111
+    if (password === new_password) {
+        return password;
+    } else if (password === old_password){
+        return password;
+    }  else {
+        return "You are a thief";
+    }
+}
+
+//output 
+console.log(login(1111));
+
+
+//write a fun to take two params
+//add two params 
+const addnum = (firstNumber,secondNumber) => {
+    let total = firstNumber + secondNumber;
+    console.log(total)
+}
+
+addnum(1,2)
